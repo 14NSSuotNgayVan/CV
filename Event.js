46f7a94aa44b482651d678dd13537c65967a0ebb
Dam Van Anh <anhmelol@gmail.com>
2023-05-24 11:30:22 +0700

    commit

---
+++ Event.js
@@ -0,0 +1,11 @@
+
+var Showmodel = (index)=>{
+    console.log(document.getElementById(`model${index}`));
+    document.getElementById(`model${index}`).style.display ='block';
+
+}
+
+var Hidemodel = (index)=>{
+        document.getElementById(`model${index}`).style.display ='none';
+
+}
